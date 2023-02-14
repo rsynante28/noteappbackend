d5ceccfb7fc3785b5e39f693179a6d287d65140d
rsynante <111683592+rsynante28@users.noreply.github.com>
2023-02-15 06:52:41 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const Hapi = require('@hapi/hapi');
+const routes = require ('./routes');
+
+const init = async () => {
+  const server = Hapi.server({
+    host: 'localhost',
+    port:5001
+  });
+
+  server.route(routes);
+  
+  await server.start();
+  console.log(`server berjalan pada ${server.info.uri}`);
+};
+
+
+init();
